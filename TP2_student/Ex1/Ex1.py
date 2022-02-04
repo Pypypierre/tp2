563abeee3046adbe02678b6351a2d845272eda29
Pypypierre <easychinese76@gmail.com>
2022-02-04 17:24:56 +0100

    TP2

--- TP2_student/Ex1/Ex1.py
+++ TP2_student/Ex1/Ex1.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 print("The current is", os.getcwd(),"\nIn this directory there is :", os.listdir(),"\nThere is :",os.listdir('test'),"in the ./test/ directory")
-os.chdir('test')
+shutil.move('IPSA.txt','test')
 os.mkdir('test2')
